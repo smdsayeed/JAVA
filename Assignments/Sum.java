f67f238eb621dea1d9c9af0572f4c19baf80ad7f
SMD.SAYEED <Smd.sayeed28@gmail.com>
2022-12-07 13:04:01 +0530

    Create Sum.java

---
+++ Assignments/Sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Sum {
+   public static void main(String[] args) {
+    System.out.println("Enter a vlaue");
+    Scanner in = new Scanner(System.in);
+    int n1 = in.nextInt();
+    System.out.println("Enter a vlaue");
+    int n2 = in.nextInt();
+    int sum = n1 + n2;
+    System.out.println(sum);
+   } 
+}
