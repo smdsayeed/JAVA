4b70eca958be392e84a3c554bb5d889b03f0f2ee
smd.sayeed <smd.sayeed28@gmail.com>
2022-12-10 11:14:39 +0530

    vs code

--- Assignments/Palindrome.java
+++ Assignments/Palindrome.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.Scanner;
 
 // To find out whether the given String is Palindrome or not.
@@ -40,3 +41,47 @@ public class Palindrome {
 
     }
 }
+=======
+import java.util.Scanner;
+
+// To find out whether the given String is Palindrome or not.
+
+
+public class Palindrome {
+    static boolean isPalindrome(String str){
+        //pointers pointing to the begining and the end of the string
+        int i =0,j= str.length()-1;
+
+        while (i < j) {
+            //If there is a mismatch
+            if(str.charAt(i)!= str.charAt(j))
+            return false;
+
+            //increment the first operator
+            //decrement the other
+            i++;
+            j--;
+        }
+
+        //given the string is
+        return true;
+    }
+    public static void main(String[] args) {
+        
+        // string input
+        String str= "hello";
+
+        // conver the string into lowercase
+        str = str.toLowerCase();
+        //passing boolean function till holding true
+
+        if (isPalindrome(str)) {
+            System.out.println(" yes");
+        } else {
+            System.out.println("NO");
+        }
+       
+
+    }
+}
+>>>>>>> a08b7d6735e224c6055d32ae10125b89d7a40e27
