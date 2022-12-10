6ad38fb5f458107cc97fe0fd626416df7ca4babf
smd.sayeed <smd.sayeed28@gmail.com>
2022-12-10 11:35:59 +0530

    modified

--- Assignments/Calculator.java
+++ Assignments/Calculator.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 
 // Take in two numbers and an operator (+, -, *, /) and calculate the value.
@@ -48,54 +47,3 @@ public class Calculator {
 
     }
 }
-=======
-import java.util.Scanner;
-
-// Take in two numbers and an operator (+, -, *, /) and calculate the value.
-
-public class Calculator {
-    public static void main(String[] args) {
-        System.out.println("Enter a number");
-        Scanner in = new Scanner(System.in);
-        
-        int n1 = in.nextInt();
-        System.out.println("Enter a number");
-        int n2 = in.nextInt();
-
-
-        System.out.println("Enter an operator like +,-,*,/");
-        char op = in.next().trim().charAt(0); //!important
-        //.trim is used to trim the space and charAt is used to check the charcter at the indexing position
-        int ans = 0;
-        if(op == '+' || op == '-' || op == '*' || op == '/'){
-           if(op =='+'){
-            ans = n1 + n2;
-            System.out.println("The sum is: " + ans);
-           }
-           if(op =='-'){
-            ans = n1 - n2;
-            System.out.println("The Subtraction is: " + ans);
-           }
-           
-           if(op =='*'){
-            ans = n1 * n2;
-            System.out.println("The Multiplication of both is: " + ans);
-           }
-           
-           if(op =='/'){
-            ans = n1 / n2;
-            System.out.println("The Remainder is: " + ans);
-           }else if(op == 'x' || op == 'X'){
-            System.out.println("invalid operator");
-           }
-        }else{
-            System.out.println("Invalid Operation");
-        }
-
-
-
-
-
-    }
-}
->>>>>>> a08b7d6735e224c6055d32ae10125b89d7a40e27
--- Assignments/Fibonacci_series.java
+++ Assignments/Fibonacci_series.java
@@ -1,21 +1,4 @@
-import java.util.Scanner;
 
-//To calculate Fibonacci Series up to n numbers.
-public class Fibonacci_series {
-    public static void main(String[] args) {
-        int a = 0,b= 1,temp;
-        System.out.println("Enter a limit to print the Fibonacci series");
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
-            temp = a+b;
-            a= b;
-            b=temp;
-            System.out.println(b);
-        }
-        System.out.print("The number at the limit provided " + b);
-    }
-}
 import java.util.Scanner;
 
 //To calculate Fibonacci Series up to n numbers.
